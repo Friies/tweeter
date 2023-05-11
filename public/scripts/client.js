013ad61f5ec54aacb6c5cda2ed54e94e92e76c28
Friies <bahram.parsa2019@gmail.com>
2023-05-11 12:34:19 -0400

    Create the submission using jQuery & loadTweet fn. - Essentially allowing for the submission of tweets when within character limit range. - Use event.preventDefault() to prevent unallowed submissions. - Use a POST Ajax request to send data to the server. & serialize the data to the server. - loadTweet fn is used to get tweets from the local '/tweets' page. 	- While deleting tweet information/data.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,37 +5,36 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-];
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   },
+// ];
 
 
 $(document).ready(function() {
   const renderTweets = function(tweets) {
-
     for (let tweet of tweets) {
       let $tweet = createTweetElement(tweet);
-    $("#tweet").append($tweet);
+      $("#tweets-container").append($tweet);
     }
   }
   
@@ -45,15 +44,15 @@ $(document).ready(function() {
     let $tweet = `
       <article class="tweet">
         <header>
-          <div class="avatar">
+          <div class="avatar-container">
+            <img src=${escape(user.avatars)}
             ${user.avatars}
           </div>
-          <span class="name">${user.name}</span>
           <span class="handle">${user.handle}</span>
         </header>
         <span class="user-tweet">${content.text}</span>
-        <span class="creation-date">${created_at}</span>
         <footer>
+          <span class="creation-date">${timeago.format(created_at)}</span>
           <div class="icons">
             <i class="fa-solid fa-flag"></i>
             <i class="fa-sharp fa-solid fa-retweet"></i>
@@ -64,5 +63,46 @@ $(document).ready(function() {
     `;
     return $tweet;
   };
-  renderTweets(data);  
+  // renderTweets(data);
+
+  const loadTweets = function() {
+    $.ajax({
+      type: "GET",
+      url: "/tweets",
+      dataType: "json",
+      success: renderTweets
+    });
+  };
+
+  loadTweets("/tweets", "GET", renderTweets);
+
+  const handleSubmitRequest = function(text) {
+    if (text.length > 140) {
+      return alert("Error, this tweet is too long.");
+    } else if (!text) {
+      return alert("Error, your tweet can't be blank.");
+    } else {
+      $.ajax({
+        url: "/tweets",
+        method: "POST",
+        data: { text },
+      })
+        .done(function () {
+          alert("Success!");
+        })
+        .fail(function () {
+          alert("Error");
+        })
+        .always(function () {
+          alert("Finished!");
+        });
+    }
+  };
+
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    $(this).serialize();
+    // console.log($(this).serialize());
+    handleSubmitRequest($("textarea").val());
+  });
 });
