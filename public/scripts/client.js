c8b31de063e5e929be936bf751fa77b859718e51
Friies <bahram.parsa2019@gmail.com>
2023-05-11 13:43:17 -0400

    Add the escape function which will prevent XSS code injection attacks.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,33 +4,13 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   },
-// ];
-
-
 $(document).ready(function() {
+  const escape = function(string) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(string));
+    return div.innerHTML;
+  };
+
   const renderTweets = function(tweets) {
     for (let tweet of tweets) {
       let $tweet = createTweetElement(tweet);
@@ -46,11 +26,12 @@ $(document).ready(function() {
         <header>
           <div class="avatar-container">
             <img src=${escape(user.avatars)}
+            <span class="name">${escape(user.name)}</span>            
             ${user.avatars}
           </div>
-          <span class="handle">${user.handle}</span>
+          <span class="handle">${escape(user.handle)}</span>
         </header>
-        <span class="user-tweet">${content.text}</span>
+        <span class="user-tweet">${escape(content.text)}</span>
         <footer>
           <span class="creation-date">${timeago.format(created_at)}</span>
           <div class="icons">
@@ -64,8 +45,8 @@ $(document).ready(function() {
     return $tweet;
   };
 
-
-  const loadTweets = function() {
+// Fetches the Tweets
+  const loadTweets = function () {
     $.ajax({
       type: "GET",
       url: "/tweets",
@@ -76,11 +57,11 @@ $(document).ready(function() {
 
   loadTweets("/tweets", "GET", renderTweets);
 
-  // const handleSubmitRequest = function(text) {
-  const formReset =function {
-    document.getElementById("tweet-form").requestFullscreen();
+  // const handleSubmitRequest = function (text) {
+  const formReset = function () {
+    document.getElementById("tweet-form").reset();
   };
-    ${"form"}.on("submit", function(event) {
+    ${"form"}.on("submit", function (event) {
       event.preventDefault();
       const text = $("textarea").length;
       
@@ -89,7 +70,6 @@ $(document).ready(function() {
     } else if (!text) {
       return alert("Error, your tweet can't be blank.");
     } else {
-
       let serializedData = $(this).serialize();
 
     $.ajax({
